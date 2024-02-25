b0f67b5df127be84f0a1616b3a95c53ef3b87174
atulit gaur <atulit23@gmail.com>
2024-02-25 19:46:33 +0530

    ne

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+from gradio_client import Client
+import json
+
+app = Flask(__name__)
+
+client = Client("https://atulit23-realvfake.hf.space/")
+phishing_client = Client("https://atulit23-phishingurls.hf.space/")
+
+@app.route('/ai-image', methods=['GET'])
+def aiImageDet():
+    try:
+        url = request.args.get('url')
+        if not url:
+            return jsonify(error='URL parameter is missing'), 400
+
+        result = client.predict(
+            url,
+            api_name="/predict"
+        )
+        print(result)
+        return jsonify(json.loads(result))
+
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+    
+@app.route('/phishing', methods=['GET'])
+def detectPhishing():
+    try:
+        url = request.args.get('url')
+        if not url:
+            return jsonify(error='URL parameter is missing'), 400
+
+        result = phishing_client.predict(
+            url,
+            api_name="/predict"
+        )
+        print(result)
+        return jsonify({"result": result})
+
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
